1f2038685d066ad5fa11fe1a0b7b44dab3bf2203
Jothikumar S Karuppasamy <Jothikumar.Skaruppasamy@sqs.com>
2020-08-23 12:39:47 -0400

    changes to settings file

--- faq_list/settings.py
+++ faq_list/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '.env/SECRET_KEY'
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['http://app-faq.herokuapp.com/websure_faq/']
 
